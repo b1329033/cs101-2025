b20b237027fca5b9eac856142d2161bd47633e8e
b1329033 <NINAHSIEH950520@GMAIL.COM>
2025-03-13 15:11:47 +0800

    main.c

--- main.c
+++ main.c
@@ -1,30 +1,76 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    FILE *fp;
-    int a[] = {0, 1, 2};
-    char b[] = "ABC";
-    float c[] = {1.1, 1.2, 1.3};
+typedef struct employee{
+    int id;
+    char name[12];
+    int age;
+    float salary;
+}employee_t;
 
-    fp = fopen("a.bin", "wb");
-    fwrite(a, sizeof(a), 1, fp);
-    fwrite(b, sizeof(b) - 1, 1, fp);  
-    fwrite(c, sizeof(c), 1, fp);
+void emp_info(employee_t emp){
+    printf("employee id=%d\n",emp.id);
+    printf("employee name=%s\n",emp.name);
+    printf("employee age=%d\n",emp.age);
+    printf("employee salary=%02f\n",emp.salary);
+}
+int emp_aveage_age(employee_t emp[],int n){
+    int sum=0;
+    for(int i=0;i<3;i++){
+        sum+=emp[i].age;
+    }
+    return sum/3;
+}
+
+void emp_writefile(employee_t emp[],int n){
+    FILE*fp=fopen("employee.bin","wb+");
+    for(int i=0;i<n;i++){
+        fwrite(&emp[i],sizeof(employee_t),1,fp);
+    }
     fclose(fp);
+    return;
+}
 
-    int ra[3];
-    char rb[3];
-    float rc[3];
+void emp_readfile(employee_t emp[]){
+    FILE*fp=fopen("employee.bin","rb");
+    int i=0;
+    while(fread(&emp[i],sizeof(employee_t),1,fp)){
+        printf("[%d] %d %s\n",i,emp[i].id,emp[i].name);
+        i++;
+    }
+    fclose(fp);
+    return;
+    }
+
+int main()
+{
+    employee_t emp[3];
+    emp[0].id=1;
+    emp[0].age=20;
+    emp[0].salary=30000.0;
+    strcpy(emp[0].name,"IU");
     
-    fp = fopen("a.bin", "rb");
-    fread(ra, sizeof(ra), 1, fp);
-    fread(rb, sizeof(rb), 1, fp);
-    fread(rc, sizeof(rc), 1, fp);
-    fclose(fp);
+    emp[1].id=2;
+    emp[1].age=26;
+    emp[1].salary=36000.0;
+    strcpy(emp[1].name,"taylor");
+    
+    emp[2].id=3;
+    emp[2].age=31;
+    emp[2].salary=90000.0;
+    strcpy(emp[2].name,"swift");
+    
+    emp_writefile(emp,3);
+    employee_t read_emp[10];
+    emp_readfile(read_emp);
+    
+    int age=emp_aveage_age(emp,3);
+    printf("Average age: %d\n\n",age);
+    
+    for(int i=0;i<3;i++){
+        emp_info(read_emp[i]);
+    }
 
-    printf("%d %d %d\n", ra[0], ra[1], ra[2]);
-    printf("%c %c %c\n", rb[0], rb[1], rb[2]);
-    printf("%.6f %.6f %.6f\n", rc[0], rc[1], rc[2]);
 
     return 0;
 }
