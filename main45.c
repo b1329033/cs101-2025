5843ce0c390118f3331f8d12aeaea08345516012
b1329033 <NINAHSIEH950520@GMAIL.COM>
2025-04-10 15:45:36 +0800

    Create main45.c

---
+++ main45.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void towerofHanoi(int a, char from_rod, char to_rod, char anx_rod){
+    if (n==1){
+        printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
+        return;
+    }
+    towerofHanoi(n-1, from_rod, aux_rod, to_rod);
+    printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
+    towerofHanoi(n-1, aux_rod, to_rod, from_rod);
+}
+int main(){
+    int n=3;
+    towerofHanoi(n, 'A', 'B', 'C');
+    return 0;
+}
